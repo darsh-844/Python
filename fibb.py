94f35ab5c601544e460690a3e40087af7e9761b2
darsh-844 <darshjaiswal2007@gmail.com>
2026-08-22 11:51:54 +0530

    Py-fibbonaci

---
+++ fibb.py
@@ -0,0 +1,17 @@
+x=int(input("Enter the number of terms: "))
+y=0
+z=1
+
+for i in range (0,x):
+    print(y)
+    y=z+y
+    z=y-z
+    
+a=int(input("Enter the term for which you want to find the fibonacci number: "))
+b=0
+c=1
+for i in range (0,a):
+    b=c+b
+    c=b-c
+print("Fibonacci number at ",a," term is ", c)
+    
