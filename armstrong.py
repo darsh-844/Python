8267f5db83667df2f44a59411449afa88a1f5840
darsh-844 <darshjaiswal2007@gmail.com>
2026-09-07 15:50:41 +0530

    Py-armstrong

--- armstrong.py
+++ armstrong.py
@@ -1,12 +1,24 @@
-a=int(input("Enter a number: "))
-sum=0
-temp=a
-while temp>0:
-    digit=temp%10
+
+# while temp>0:
+#     digit=temp%10
+#     x=len(str(a))
+#     sum+=digit**x
+#     temp//=10
+
+def armstrong(a):
+    sum=0
+    temp=a
     x=len(str(a))
-    sum+=digit**x
-    temp//=10
+    while temp>0:
+        digit=temp%10
+        sum+=digit**x
+        temp//=10
+    return sum
+
+a=int(input("Enter a number: "))
+sum=armstrong(a)
 if a==sum:
-    print(a,"is an Armstrong number")
+     print(a,"is an Armstrong number")
 else:
-    print(a,"is not an Armstrong number")   
+     print(a,"is not an Armstrong number")   
+
